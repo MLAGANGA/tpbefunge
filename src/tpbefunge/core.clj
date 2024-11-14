36d85b173b23179362f4ca3458346e737e8e5291
MLAGANGA <mlaganga@fi.uba.ar>
2024-11-13 21:16:51 -0300

    Desarrollo integral del proyecto. Trabajo con las pilas y archivos.

--- src/tpbefunge/core.clj
+++ src/tpbefunge/core.clj
@@ -1,4 +1,4 @@
-(ns tpbefunge.core
+(ns befunge-interpreter.core
   (:require [clojure.java.io :as io])
   (:import [java.util Random]))
 
@@ -20,25 +20,82 @@
       (reset! grilla (vec (map #(vec (concat % (repeat (- max-ancho (count %)) \space))) lineas))))))
 
 (defn envolver [val max]
-  (mod (+ val max) max))  
+  (mod (+ val max) max))
 
 (defn mover []
   (let [[desplazamiento-x desplazamiento-y] (direcciones @direccion)]
-    (swap! x #(envolver (+ % desplazamiento-x) (count (first @grilla)))) 
-    (swap! y #(envolver (+ % desplazamiento-y) (count @grilla)))))  
+    (swap! x #(envolver (+ % desplazamiento-x) (count (first @grilla))))
+    (swap! y #(envolver (+ % desplazamiento-y) (count @grilla)))))
 
 (defn apilar [val]
+  "Apilar un valor en la pila."
   (swap! pila conj val))
 
 (defn desapilar []
+  "Desapilar un valor de la pila; si la pila está vacía, devuelve 0."
   (let [val (first @pila)]
     (swap! pila rest)
     (or val 0)))
 
+(defn procesar-instruccion [instr]
+  "Procesa una instrucción Befunge."
+  (if @en-comillas?
+    (if (= instr \") (swap! en-comillas? not) (apilar (int instr)))
+    (case instr
+      \> (reset! direccion :right)
+      \< (reset! direccion :left)
+      \^ (reset! direccion :up)
+      \v (reset! direccion :down)
+      \+ (apilar (+ (desapilar) (desapilar)))
+      \- (let [a (desapilar) b (desapilar)] (apilar (- b a)))
+      \* (apilar (* (desapilar) (desapilar)))
+      \/ (let [a (desapilar) b (desapilar)] (apilar (if (= a 0) 0 (quot b a))))
+      \% (let [a (desapilar) b (desapilar)] (apilar (if (= a 0) 0 (mod b a))))
+      \! (apilar (if (zero? (desapilar)) 1 0))
+      \` (let [a (desapilar) b (desapilar)] (apilar (if (> b a) 1 0)))
+      \? (reset! direccion (rand-nth (keys direcciones)))
+      \_ (reset! direccion (if (zero? (desapilar)) :right :left))
+      \| (reset! direccion (if (zero? (desapilar)) :down :up))
+      \" (swap! en-comillas? not)
+      \: (apilar (or (first @pila) 0))
+      \\ (let [a (desapilar) b (desapilar)] (apilar a) (apilar b))
+      \$ (desapilar)
+      \. (let [num (desapilar)] (when num (print (str num " ")))) 
+      \, (let [chr (desapilar)] (when chr (print (char chr))))     
+      \# (mover) 
+      \= (let [a (desapilar) b (desapilar)] (apilar (if (= b a) 1 0)))
+      \p (let [y (desapilar) x (desapilar) v (desapilar)]
+           (swap! grilla update-in [y x] (constantly (char v))))
+      \g (let [y (desapilar) x (desapilar)]
+           (when (and (< y (count @grilla)) (< x (count (first @grilla))))
+             (apilar (int (get-in @grilla [y x])))))
+      \& (apilar (Integer/parseInt (read-line)))
+      \~ (apilar (int (first (read-line))))
+      (when (Character/isDigit instr)
+        (apilar (Character/digit instr 10))))))
 
-;; Ciclo principal de ejecucion, termina cuando aparece @
+(defn paso []
+  (procesar-instruccion (get-in @grilla [@y @x]))
+  (mover))
+
+(defn ejecutar-con-limite [filename]
+  (leer-archivo filename)
+  (loop [pasos 0]
+    (when (and (not= (get-in @grilla [@y @x]) \@) (< pasos max-pasos))
+      (paso)
+      (recur (inc pasos))))
+  (if (= (get-in @grilla [@y @x]) \@)
+    (println "\nPrograma finalizado.")
+    (println "\nPrograma detenido después de alcanzar el máximo de pasos para prevenir bucles infinitos.")))
+
+;; Ciclo principal de ejecucion, se detiene en  @
 (defn ejecutar [filename]
   (leer-archivo filename)
   (while (not= (get-in @grilla [@y @x]) \@)
     (paso))
   (println "\nPrograma finalizado."))
+
+(defn -main [& args]
+  (if (or (empty? args) (not (clojure.string/ends-with? (first args) ".bf")))
+    (println "ERROR: ¡Especifica un archivo .bf como entrada!")
+    (ejecutar (first args))))
