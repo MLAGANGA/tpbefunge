41db9920fd672161cb13dcb6a5d5f040cc1ae14f
Gonzalo <golmos@fi.uba.ar>
2024-11-26 19:06:09 -0300

    funcionando 99%. Caracter 0 imprimiendo en prime.bf. Faltan tests

--- src/tpbefunge/core.clj
+++ src/tpbefunge/core.clj
@@ -1,98 +1,143 @@
 (ns tpbefunge.core
-  (:require [clojure.java.io :as io])
-  (:import [java.util Random]))
+    (:require [clojure.java.io :as io])
+    (:import [java.util Random]))
 
-;; Estado del intérprete
-(def grilla (atom []))
-(def x (atom 0))
-(def y (atom 0))
-(def direccion (atom :right))
-(def pila (atom '()))
-(def en-comillas? (atom false))
+;; Parámetros y configuraciones
 (def direcciones {:right [1 0], :left [-1 0], :up [0 -1], :down [0 1]})
-(def aleatorio (Random.))
-(def max-pasos 1000)
+(def random (Random.))
+(def max-iter 200000)    ;; Para evitar bucles infinitos
+(def dimensiones {:x 80 :y 25}) ;; Dimensiones de la grilla
 
+;; Lectura del archivo y generación de la grilla
 (defn leer-archivo [filename]
-  (with-open [reader (io/reader filename)]
-    (let [lineas (doall (line-seq reader))
-          max-ancho (apply max (map count lineas))]
-      (reset! grilla (vec (map #(vec (concat % (repeat (- max-ancho (count %)) \space))) lineas))))))
+      (with-open [reader (io/reader filename)]
+                 (let [max-ancho (:x dimensiones)
+                       max-alto (:y dimensiones)
+                       lineas (take max-alto (doall (line-seq reader)))
+                       grilla-aux (vec (map #(vec (concat (take max-ancho %)
+                                                          (repeat (- max-ancho (count %)) \space)))
+                                            lineas))]
+                      (vec (concat grilla-aux
+                                   (repeat (- max-alto (count grilla-aux))
+                                           (vec (repeat max-ancho \space))))))))
 
+;; Utilidades
 (defn envolver [val max]
-  (mod (+ val max) max))
+      (mod (+ val max) max))
 
-(defn mover []
-  (let [[desplazamiento-x desplazamiento-y] (direcciones @direccion)]
-    (swap! x #(envolver (+ % desplazamiento-x) (count (first @grilla))))
-    (swap! y #(envolver (+ % desplazamiento-y) (count @grilla)))))
+(defn mover [direccion pc]
+      (let [[dx dy] (get direcciones direccion)
+            [x y] pc
+            limite-ancho (:x dimensiones)
+            limite-alto (:y dimensiones)]
+           [(envolver (+ x dx) limite-ancho) (envolver (+ y dy) limite-alto)]))
 
-(defn apilar [val]
-  (swap! pila conj val))
+;; Operaciones con la pila
+(defn crear-pila [] '())
 
-(defn desapilar []
-  (let [val (first @pila)]
-    (swap! pila rest)
-    (or val 0)))
+(defn apilar [pila val]
+      (conj pila val))
 
-(defn procesar-instruccion [instr]
-  (if @en-comillas?
-    (if (= instr \") (swap! en-comillas? not) (apilar (int instr)))
-    (case instr
-      \> (reset! direccion :right)
-      \< (reset! direccion :left)
-      \^ (reset! direccion :up)
-      \v (reset! direccion :down)
-      \+ (apilar (+ (desapilar) (desapilar)))
-      \- (let [a (desapilar) b (desapilar)] (apilar (- b a)))
-      \* (apilar (* (desapilar) (desapilar)))
-      \/ (let [a (desapilar) b (desapilar)] (apilar (if (= a 0) 0 (quot b a))))
-      \% (let [a (desapilar) b (desapilar)] (apilar (if (= a 0) 0 (mod b a))))
-      \! (apilar (if (zero? (desapilar)) 1 0))
-      \` (let [a (desapilar) b (desapilar)] (apilar (if (> b a) 1 0)))
-      \? (reset! direccion (rand-nth (keys direcciones)))
-      \_ (reset! direccion (if (zero? (desapilar)) :right :left))
-      \| (reset! direccion (if (zero? (desapilar)) :down :up))
-      \" (swap! en-comillas? not)
-      \: (apilar (or (first @pila) 0))
-      \\ (let [a (desapilar) b (desapilar)] (apilar a) (apilar b))
-      \$ (desapilar)
-      \. (let [num (desapilar)] (when num (print (str num " ")))) 
-      \, (let [chr (desapilar)] (when chr (print (char chr))))    
-      \# (mover)  
-      \= (let [a (desapilar) b (desapilar)] (apilar (if (= b a) 1 0)))
-      \p (let [y (desapilar) x (desapilar) v (desapilar)]
-           (swap! grilla update-in [y x] (constantly (char v))))
-      \g (let [y (desapilar) x (desapilar)]
-           (when (and (< y (count @grilla)) (< x (count (first @grilla))))
-             (apilar (int (get-in @grilla [y x])))))
-      \& (apilar (Integer/parseInt (read-line)))
-      \~ (apilar (int (first (read-line))))
-      (when (Character/isDigit instr)
-        (apilar (Character/digit instr 10))))))
+(defn desapilar [pila]
+      (if (seq pila)
+        [(peek pila) (pop pila)]
+        [0 pila]))
 
-(defn paso []
-  (procesar-instruccion (get-in @grilla [@y @x]))
-  (mover))
+(defn operacion-binaria [pila f]
+      (let [[a p1] (desapilar pila)
+            [b p2] (desapilar p1)]
+           (apilar p2 (f b a))))
 
-(defn ejecutar-con-limite [filename]
-  (leer-archivo filename)
-  (loop [pasos 0]
-    (when (and (not= (get-in @grilla [@y @x]) \@) (< pasos max-pasos))
-      (paso)
-      (recur (inc pasos))))
-  (if (= (get-in @grilla [@y @x]) \@)
-    (println "\nPrograma finalizado.")
-    (println "\nPrograma detenido después de alcanzar el máximo de pasos.")))
+;; Procesamiento de instrucciones
+(defn procesar-instruccion [mapa-estado instr]
+      (let [{:keys [pila direccion grilla en-comillas? pc]} mapa-estado]
+           (if en-comillas?
+             (if (= instr \")
+               (assoc mapa-estado :en-comillas? false)
+               (assoc mapa-estado :pila (apilar pila (int instr))))
+             (case instr
+                   \> (assoc mapa-estado :direccion :right)
+                   \< (assoc mapa-estado :direccion :left)
+                   \^ (assoc mapa-estado :direccion :up)
+                   \v (assoc mapa-estado :direccion :down)
+                   \+ (assoc mapa-estado :pila (operacion-binaria pila +))
+                   \- (assoc mapa-estado :pila (operacion-binaria pila -))
+                   \* (assoc mapa-estado :pila (operacion-binaria pila *))
+                   \/ (let [[a p1] (desapilar pila)
+                            [b p2] (desapilar p1)]
+                           (assoc mapa-estado :pila (apilar p2 (if (zero? a) 0 (quot b a)))))
+                   \% (let [[a p1] (desapilar pila)
+                            [b p2] (desapilar p1)]
+                           (assoc mapa-estado :pila (apilar p2 (if (zero? a) 0 (mod b a)))))
+                   \! (let [[v p] (desapilar pila)]
+                           (assoc mapa-estado :pila (apilar p (if (zero? v) 1 0))))
+                   \` (let [[a p1] (desapilar pila)
+                            [b p2] (desapilar p1)]
+                           (assoc mapa-estado :pila (apilar p2 (if (> b a) 1 0))))
+                   \? (assoc mapa-estado :direccion (rand-nth (keys direcciones)))
+                   \_ (let [[v p] (desapilar pila)]
+                           (assoc mapa-estado :direccion (if (zero? v) :right :left) :pila p))
+                   \| (let [[v p] (desapilar pila)]
+                           (assoc mapa-estado :direccion (if (zero? v) :down :up) :pila p))
+                   \" (assoc mapa-estado :en-comillas? true)
+                   \: (assoc mapa-estado :pila (apilar pila (or (peek pila) 0)))
+                   \\ (let [[a p1] (desapilar pila)
+                            [b p2] (desapilar p1)]
+                           (assoc mapa-estado :pila (apilar (apilar p2 a) b)))
+                   \$ (assoc mapa-estado :pila (pop pila))
+                   \. (let [[num p] (desapilar pila)]
+                           (do (print (str num " ")) (assoc mapa-estado :pila p)))
+                   \, (let [[chr p] (desapilar pila)]
+                           (do (print (char chr)) (assoc mapa-estado :pila p)))
+                   \# (assoc mapa-estado :pc (mover direccion pc))
+                   \p (let [[y p1] (desapilar pila)
+                            [x p2] (desapilar p1)
+                            [v p3] (desapilar p2)]
+                           ;(assoc mapa-estado :grilla (assoc-in grilla [y x] (char v))
+                            (assoc mapa-estado :grilla (assoc-in grilla [(envolver y (:y dimensiones))
+                                                                         (envolver x (:x dimensiones))] (char v))
+                                              :pila p3))
+                   \g (let [[y p1] (desapilar pila)
+                            [x p2] (desapilar p1)]
+                           (assoc mapa-estado :pila (apilar p2 (if(or
+                                                                    (> x (- (:x dimensiones) 1))
+                                                                    (> y (- (:y dimensiones) 1)))
+                                                                 0
+                                                                 (int (get-in grilla [y x]))))))
+                   \& (assoc mapa-estado :pila (apilar pila (Integer. (read-line))))
+                   \~ (assoc mapa-estado :pila (apilar pila (int (first (read-line)))))
+                   \space mapa-estado
+                   (if (Character/isDigit instr)
+                     (assoc mapa-estado :pila (apilar pila (Character/digit instr 10)))
+                     mapa-estado)))))
 
-;; Ciclo principal de ejecucion, se detiene en el simbolo "@"
+(defn paso [mapa-estado instr]
+      (let [mapa-actualizado (procesar-instruccion mapa-estado instr)
+            nuevo-pc (mover (:direccion mapa-actualizado) (:pc mapa-actualizado))]
+           (assoc mapa-actualizado :pc nuevo-pc)))
+
+;; Inicialización y ejecución
+(defn crear-mapa-estado [filename]
+      {:pila (crear-pila)
+       :grilla (leer-archivo filename)
+       :direccion :right
+       :en-comillas? false
+       :pc [0 0]})
+
+(defn obtener-instr [mapa-estado]
+      (let [pc (:pc mapa-estado)]
+           (get-in (:grilla mapa-estado) [(second pc) (first pc)])))
+
 (defn ejecutar [filename]
-  (leer-archivo filename)
-  (while (not= (get-in @grilla [@y @x]) \@)
-    (paso))
-  (println "\nPrograma finalizado."))
+      (let [mapa-estado (crear-mapa-estado filename)]
+           (loop [iter 0
+                  estado mapa-estado]
+                 (let [instr (obtener-instr estado)]
+                      (if (and (not= instr \@) (< iter max-iter))
+                        (recur (inc iter) (paso estado instr))
+                        (println "\nPrograma finalizado."))))))
 
 (defn -main [& args]
-  (if (or (empty? args) (not (clojure.string/ends-with? (first args) ".bf")))
-    (println "ERROR: ¡Especifica un archivo .bf como entrada!")
-    (ejecutar (first args))))
+      (if (or (empty? args) (not (clojure.string/ends-with? (first args) ".bf")))
+        (println "ERROR: ¡Especifica un archivo .bf como entrada!")
+        (ejecutar (first args))))
--- test/tpbefunge/pila_test.clj
+++ test/tpbefunge/pila_test.clj
@@ -5,17 +5,17 @@
 
 (deftest test-crear-pila
   (testing "Crear una pila vacía"
-    (is (= '() @(crear-pila)))))
+    (is (= '() (crear-pila)))))
 
 (deftest test-pila-apilar
   (testing "Testeando apilar en pila vacia"
     (let [pila (crear-pila)]
     (apilar pila 1)
-    (is (= '(1) @pila))))
+    (is (= '(1) pila))))
 
   (testing "Testeando apilar en pila poblada"
-    (let [pila (crear-pila)]
-    (apilar pila 1)
-    (apilar pila 2)
-    (is (= '(2 1) @pila))))
+    (let [pila (crear-pila)
+          p2 (apilar pila 1)
+          p3 (apilar p2 2)]
+    (is (= '(2 1) p3))))
 )
